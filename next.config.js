8dba999e0fa828f9e968693b2f14693a5613763d
Nayeem <nayeem.gmit@gmail.com>
2023-12-08 13:40:55 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
+  webpack: config => {
+    config.resolve.fallback = { fs: false }
+    return config
+  }
+}
 
 module.exports = nextConfig
